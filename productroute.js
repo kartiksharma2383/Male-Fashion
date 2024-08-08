3d529f7406ad6fcd5b84ad518cd04c0efd3cc6fd
kartiksharma2383 <kartiksharma2383@gmail.com>
2024-08-08 10:48:44 +0530

    Add files via upload

---
+++ productroute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const getproductcontroller = require("../controllers/products/getproductcontroller");
+const router = express.Router();
+router.get("/", getproductcontroller);
+router.get("/category/:category",getproductcontroller);
+router.get("/subcategory/:subcategory",getproductcontroller);
+router.get("/name/:name",getproductcontroller);
+router.get("/id/:id",getproductcontroller);
+router.get("/random",getproductcontroller);
+router.get("/top-rated",getproductcontroller);
+router.get("/lowtohigh",getproductcontroller);
+module.exports = router;
