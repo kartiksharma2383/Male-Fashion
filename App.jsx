e0d31150900bd6f5604fc99217eb2a2c44f6303a
kartiksharma2383 <kartiksharma2383@gmail.com>
2024-08-10 09:16:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherCard from './Components/WeatherCard';
+const App = () => {
+  return (
+<>
+<WeatherCard
+data="40"/>
+      <WeatherCard
+      data="20" />
+</>
+)
+}
+export default App
